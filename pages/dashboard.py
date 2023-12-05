c09c896156629f3cbcff980cc7b791e85ef99d9d
spike <azamtoiri@gmail.com>
2023-12-05 11:51:01 +0300

    STUFF: intermediate commit

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,14 +1,17 @@
+import random
 
+import plotly.graph_objs as go
 from flet import *
-from utils.colors import BLUE, DB_TEXT
 
-from service.auth import load_token, get_name, revoke_token
+from service.auth import load_token, get_name
+from utils.colors import BLUE
 
 
 class Dashboard(Container):
     def __init__(self, page: Page):
         super().__init__()
 
+        fig = self.get_fig()
         page.padding = 0
         self.expand = True
         self.current_user_name = get_name(load_token())
@@ -17,8 +20,9 @@ class Dashboard(Container):
             spacing=0,
             controls=[
                 Container(
-                    width=220,
+                    width=200,
                     bgcolor=BLUE,
+                    # using padding for correcting position of objects in container
                     padding=padding.only(top=20, left=10, right=10),
                     content=Column(
                         controls=[
@@ -26,90 +30,77 @@ class Dashboard(Container):
                                 controls=[
                                     Icon(
                                         icons.PERSON,
-                                        size=50
+                                        size=50,
                                     )
                                 ]
                             ),
-                            Divider(
-                                color='#9caede',
-                                height=0.5,
-                                thickness=.5
-                            ),
+
+                            Divider(color='#9caede', height=0.5, thickness=0.5),
+
                             Container(
-                                Text(
-                                    value="Dashboard",
-                                    size=14,
-                                    color='white'
-                                ),
-                            ),
-                            Divider(
-                                color='#9caede',
-                                height=0.5,
-                                thickness=.5
-                            ),
-                            Container(
-                                Text(
-                                    value="Utilities",
-                                    size=14,
-                                    color='white'
-                                ),
-                            ),
-                            Divider(
-                                color='#9caede',
-                                height=0.5,
-                                thickness=.5
+                                content=Text(value='Dashboard', size=14, color='WHITE')
                             ),
 
+                            Divider(color='#9caede', height=0.5, thickness=0.5),
+
+                            Container(
+                                content=Text(value='Utilities', size=14, color='WHITE')
+                            ),
+
+                            Divider(color='#9caede', height=0.5, thickness=0.5),
+
                             Row(
-                                alignment='center',
+                                alignment=alignment.center,
                                 controls=[
                                     Container(
                                         alignment=alignment.center,
                                         height=35,
                                         width=35,
-                                        bgcolor='#9caede',
+                                        bgcolor='9caede',
                                         border_radius=20,
                                         content=Icon(
                                             icons.ARROW_BACK_IOS,
                                             size=12,
                                         )
-                                    ),
+                                    )
                                 ]
-                            ),
-                            Container(
-                                height=200,
-                                bgcolor='#3c5ec1',
-                                border_radius=5,
-                                alignment=alignment.center,
-                                content=Column(
-                                    horizontal_alignment='center',
-                                    alignment='center',
-                                    controls=[
-                                        Text(
-                                            value="This site was designed by: @1MrNewton on YouTube",
-                                            text_align='center',
-                                            color='#9caede'
-                                        ),
-                                        Container(
-                                            on_click=lambda _: (revoke_token(
-                                                load_token()), self.page.go('/login')),
-                                            alignment=alignment.center,
-                                            height=35, width=110,
-                                            border_radius=5,
-                                            bgcolor='#66FFFFFF',
-                                            content=Text(
-                                                value="Log Out",
-                                                color='black',
-                                                size=14,
-                                                weight=FontWeight.W_600
-                                            )
-                                        )
-                                    ]
-                                )
-
                             )
                         ]
                     )
-                ),
+                )
             ]
         )
+
+    def get_fig(self):
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+        spendings = [random.randint(100, 1000) for _ in range(len(months))]
+
+        fig = go.Figure()
+
+        fig.add_trace(go.Scatter(
+            x=months,
+            y=spendings,
+            mode='lines+markers',
+            line=dict(shape='spline', width=5,
+                      color='rgba(78, 115, 223, 0.5)'),
+            fill='tozeroy',
+            fillcolor="rgba(78, 115, 223, 0.3)",
+            marker=dict(size=9, color='rgba(78, 115, 223, 1)',
+                        line=dict(width=1, color='rgba(78, 115, 223, 1)')),
+            hovertemplate="""<b>%{x}</b><br>
+        <span>Spendings: %{y}</span>"""
+        ))
+
+        fig.update_xaxes(showgrid=False, gridwidth=0.1, gridcolor='LightPink')
+        fig.update_layout(
+            title='Monthly Expenses',
+            xaxis_title='Month',
+            yaxis_title='Spending ($)',
+            showlegend=False,
+            # plot_bgcolor='rgba(0,0,0,0)',
+            # paper_bgcolor='rgba(0,0,0,0)',
+            # grid=dict(type='dot')
+        )
+        return fig
