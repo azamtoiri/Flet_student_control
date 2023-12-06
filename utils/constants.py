27324eb40b911c47c6119f8cf0389fb9e6567e96
spike <azamtoiri@gmail.com>
2023-12-06 08:00:34 +0300

    ADD: new constant for sheet bg

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,9 @@
+from flet import colors
+
 LOGO_PATH = '../assets/Fox_Hub_logo.png'
 BLUE = '9caede'
 LEFT_COL_COLOR = '#363740'
 RIGHT_COL_COLOR = '#F7F8FC'
 SHEET = '#9FA2B4'
+
+SHEET_BG_COLOR = colors.with_opacity(0.1, color=SHEET)
