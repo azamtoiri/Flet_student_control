7a93ef969b005a5ae02c4f78e27100c53c693234
spike <azamtoiri@gmail.com>
2023-12-14 18:28:43 +0300

    CHANGES: centered objects on `WelcomeView`

--- pages/common_views.py
+++ pages/common_views.py
@@ -10,23 +10,28 @@ class WelcomeView(View):
         super().__init__()
         self.route = "/welcome"
         self.horizontal_alignment = CrossAxisAlignment.CENTER
+        self.vertical_alignment = MainAxisAlignment.CENTER
 
-        self.logo_image = Image()
-        self.logo_image.src = LOGO_PATH
+        self.logo_image = Image(src=LOGO_PATH)
         self.logo_image.width = 100
         self.logo_image.height = 100
+        self.logo_image.expand = True
 
         self.logo_text = Text()
         self.logo_text.value = "FoxHub"
-        self.logo_text.size = 21
         self.logo_text.width = FontWeight.BOLD
+        self.logo_text.text_align = TextAlign.CENTER
         self.logo_text.color = colors.with_opacity(1, "White")
+        self.logo_text.size = 30
+        self.logo_text.expand = True
 
         self.welcome_text = Text()
         self.welcome_text.value = "Добро пожаловать"
         self.welcome_text.size = 34
         self.welcome_text.width = FontWeight.W_500
+        self.welcome_text.text_align = TextAlign.CENTER
         self.welcome_text.color = colors.with_opacity(1, "White")
+        self.welcome_text.expand = True
 
         self.login_button = ElevatedButton()
         self.login_button.text = 'Войти'
@@ -41,16 +46,21 @@ class WelcomeView(View):
         content = Column()
         content.width = 400
         content.alignment = MainAxisAlignment.CENTER
-        content.controls.append(self.logo_image)
+        content.controls.append(Row([self.logo_image]))
         content.controls.append(Row([self.logo_text]))
         content.controls.append(Row([self.welcome_text]))
         content.controls.append(Row([self.login_button]))
         content.controls.append(Row([self.register_button]))
 
         container = Container()
+        container.bgcolor = colors.TRANSPARENT
+        container.border_radius = 8
         container.content = content
-        container.border = border.all(1, color=colors.TRANSPARENT)
-        container.expand = True
+        container.width = 450
+        container.height = 650
+        container.padding = padding.all(40)
+        container.border = border.all(1, colors.TRANSPARENT)
+
         self.controls.append(container)
 
 
