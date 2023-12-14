78f41e88da03360d7b7d238218f8429464cbb657
spike <azamtoiri@gmail.com>
2023-12-14 18:10:45 +0300

    ADD: Creating `TestRegisterView`

--- pages/common_views.py
+++ pages/common_views.py
@@ -1,7 +1,7 @@
 from flet import *
 
 from utils.constants import LOGO_PATH
-from utils.customs import CustomInputField
+from utils.customs import CustomInputField, MixedView
 
 
 class WelcomeView(View):
@@ -123,54 +123,39 @@ class RegisterView(LoginView):
         self.login_button.text = 'Уже есть аккаунт?'
 
 
-class TestLogin(View):
+class TestLogin(MixedView):
     def __init__(self):
         super().__init__()
         self.route = '/login2'
-        self.horizontal_alignment = CrossAxisAlignment.CENTER
-        self.vertical_alignment = MainAxisAlignment.CENTER
 
-        self.logo_icon = Image(src=LOGO_PATH)
-        self.logo_icon.width = 100
-        self.logo_icon.height = 100
-        self.logo_icon.expand = True
-
-        self.logo_text = Text()
-        self.logo_text.value = 'FoxHub'
-        self.logo_text.weight = FontWeight.BOLD
-        self.logo_text.text_align = TextAlign.CENTER
-        self.logo_text.color = colors.BLACK
-        self.logo_text.size = 30
-        self.logo_text.expand = True
-
-        self.title = Text()
-        self.title.value = "Вход"
-        self.title.style = TextThemeStyle.TITLE_MEDIUM
-        self.title.text_align = TextAlign.CENTER
-        self.title.expand = True
-
+        # region: InputFields
         self.username_field = CustomInputField(False, 'username')
 
         self.password_field = CustomInputField(True, 'password')
+        # endregion
 
+        # region: Buttons
         self.login_button = ElevatedButton()
         self.login_button.text = "Войти"
-        self.login_button.icon = icons.APP_REGISTRATION
+        self.login_button.icon = icons.LOGIN
         self.login_button.width = 400
         self.login_button.height = 45
         self.login_button.expand = True
 
+        self.create_account_button = Container()
+        self.create_account_button.content = Text(
+            "Регистрация", size=15, color=colors.with_opacity(0.50, colors.BLUE))
+        self.create_account_button.alignment = alignment.center
+        self.create_account_button.width = 150
+        self.create_account_button.height = 45
+        # endregion
+
+        # region: Texts
         self.text_dont_not_registered = Text()
         self.text_dont_not_registered.value = "Еще нет аккаунта?"
         self.text_dont_not_registered.size = 15
         self.text_dont_not_registered.color = colors.with_opacity(0.50, colors.BLACK)
-
-        self.create_account_button = Container()
-        self.create_account_button.alignment = alignment.center
-        self.create_account_button.width = 150
-        self.create_account_button.height = 45
-        self.create_account_button.content = Text(
-            "Регистрация", color=colors.with_opacity(0.50, colors.BLUE))
+        # endregion
 
         content = Column()
         content.width = 400
@@ -181,13 +166,11 @@ class TestLogin(View):
         content.controls.append(self.username_field)
         content.controls.append(self.password_field)
         content.controls.append(Row([self.login_button]))
-
+        #
         content.controls.append(Row(
             vertical_alignment=CrossAxisAlignment.CENTER,
             alignment=MainAxisAlignment.CENTER,
-            controls=[self.text_dont_not_registered, self.create_account_button]
-        )
-        )
+            controls=[self.text_dont_not_registered, self.create_account_button]))
 
         container = Container()
         container.bgcolor = colors.WHITE
@@ -197,5 +180,56 @@ class TestLogin(View):
         container.height = 650
         container.padding = padding.all(40)
         container.border = border.all(1, colors.TRANSPARENT)
+
+        self.controls.append(container)
+
+
+class TestRegisterView(MixedView):
+    def __init__(self):
+        super().__init__()
+        # view settings
+        self.route = '/register'
+
+        self.title.value = 'Регистрация'
+
+        self.register_button = ElevatedButton()
+        self.register_button.text = 'Создать аккаунт'
+        self.register_button.width = 400
+        self.register_button.height = 45
+
+        self.login_button = Container()
+        self.login_button.alignment = alignment.center
+        self.login_button.content = Text(
+            value='Войти', color=colors.with_opacity(0.5, colors.BLUE)
+        )
+
+        self.already_hav_account = Text("У  вас уже есть аккаунт")
+        self.already_hav_account.size = 15
+        self.already_hav_account.color = colors.with_opacity(0.5, colors.BLACK)
+
+        content = Column()
+        content.width = 400
+        content.alignment = MainAxisAlignment.CENTER
+        content.controls.append(Row([self.logo_icon]))
+        content.controls.append(Row([self.logo_text]))
+        content.controls.append(Row([self.title]))
+        # content.controls.append(self.username_field)
+        # content.controls.append(self.password_field)
+        content.controls.append(Row([self.register_button]))
+        #
+        content.controls.append(Row(
+            vertical_alignment=CrossAxisAlignment.CENTER,
+            alignment=MainAxisAlignment.CENTER,
+            controls=[self.already_hav_account, self.login_button]))
+
+        container = Container()
+        container.bgcolor = colors.WHITE
+        container.border_radius = 8
+        container.content = content
+        # container.width = 450
+        # container.height = 650
+        container.padding = padding.all(40)
+        container.border = border.all(1, colors.TRANSPARENT)
+        container.expand = True
 
         self.controls.append(container)
