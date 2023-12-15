7318846b597f4085f87a09c3089e3b3cb3c20e0c
spike <azamtoiri@gmail.com>
2023-12-15 23:22:07 +0300

    DONE: refactored code structure

--- pages/register_view.py
+++ pages/register_view.py
@@ -7,8 +7,8 @@ from utils.constants import LOGO_PATH
 class SignUp(CustomContainer):
     def __init__(self, page: ft.Page):
         super().__init__(page)
-
         self.page = page
+
         self.surname = CustomInputField(False, "Фамилия")
         self.name = CustomInputField(False, "Имя")
         self.second_name = CustomInputField(False, "Отчество")
@@ -48,68 +48,59 @@ class SignUp(CustomContainer):
         self.content.width = 884
         self.content.height = 810
         self.content.bgcolor = ft.colors.with_opacity(1, "white")
-        self.content.border_radius = 10
+        self.content.border_radius = 8
         self.content.padding = 40
 
-        content_controls = [
-            ft.Row(
-                alignment=ft.MainAxisAlignment.END,
-                controls=[
-                    self.scheme_change_buttons[2],
-                    self.scheme_change_buttons[3],
-                ]
-            ),
-            self.logo,
-            ft.Text(
-                "FoxHub",
-                size=21,
-                weight=ft.FontWeight.W_800,
-                color=ft.colors.with_opacity(1, "Black")
-            ),
-            ft.Text(
-                "Регистрация",
-                size=35,
-                weight=ft.FontWeight.W_500,
-                color=ft.colors.with_opacity(0.85, "Black")
-            ),
-            ft.Row(
-                vertical_alignment=ft.CrossAxisAlignment.CENTER,
-                alignment=ft.MainAxisAlignment.CENTER,
-                controls=[
-                    ft.Column(
-                        height=400,
-                        width=250,
-                        controls=[
-                            self.surname,
-                            self.name,
-                            self.second_name,
-                            self.group,
-                        ]
-                    ),
-                    ft.Column(
-                        height=400,
-                        width=250,
-                        controls=[
-                            self.age,
-                            self.email,
-                            self.password,
-                            self.password2,
-                        ]
-                    ),
-                ]
-            ),
-            self.submit_button,
-            ft.Row(
-                vertical_alignment=ft.CrossAxisAlignment.CENTER,
-                alignment=ft.MainAxisAlignment.CENTER,
-                controls=[
-                    self.hav_account,
-                    self.login_button,
-                ]
-            )
-        ]
+        # region: Texts
+        text_foxhub = ft.Text("FoxHub")
+        text_foxhub.size = 21
+        text_foxhub.weight = ft.FontWeight.W_800
+        text_foxhub.color = ft.colors.with_opacity(1, "black")
 
-        self.content.content = ft.Column()
-        self.content.content.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-        self.content.content.controls = content_controls
+        text_login = ft.Text("Регистрация")
+        text_login.size = 35
+        text_login.weight = ft.FontWeight.W_800
+        text_login.color = ft.colors.with_opacity(0.85, "Black")
 
+        # endregion
+
+        change_color_scheme_buttons = ft.Row()
+        change_color_scheme_buttons.alignment = ft.MainAxisAlignment.END
+        change_color_scheme_buttons.controls.append(self.scheme_change_buttons[2])
+        change_color_scheme_buttons.controls.append(self.scheme_change_buttons[3])
+
+        left_fields_controls = ft.Column()
+        left_fields_controls.width = 260
+        left_fields_controls.height = 355
+        left_fields_controls.controls.append(self.surname)
+        left_fields_controls.controls.append(self.name)
+        left_fields_controls.controls.append(self.second_name)
+        left_fields_controls.controls.append(self.group)
+
+        right_fields_controls = ft.Column()
+        # right_fields_controls.expand = True
+        right_fields_controls.width = 260
+        right_fields_controls.height = 355
+        right_fields_controls.controls.append(self.age)
+        right_fields_controls.controls.append(self.email)
+        right_fields_controls.controls.append(self.password)
+        right_fields_controls.controls.append(self.password2)
+
+        row_no_account = ft.Row()
+        row_no_account.vertical_alignment = ft.CrossAxisAlignment.CENTER
+        row_no_account.alignment = ft.MainAxisAlignment.CENTER
+        row_no_account.controls = [self.hav_account, self.login_button]
+
+        content_controls = ft.Column(horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+        content_controls.controls.append(change_color_scheme_buttons)
+        content_controls.controls.append(self.logo)
+        content_controls.controls.append(text_foxhub)
+        content_controls.controls.append(ft.Row(
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[left_fields_controls, right_fields_controls]
+        ))
+        content_controls.controls.append(self.submit_button)
+        content_controls.controls.append(row_no_account)
+
+        self.content.content = content_controls
