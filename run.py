e09aa66a3cbaa3321aff0f9235483f4871e9ea0f
spike <azamtoiri@gmail.com>
2023-12-16 13:56:00 +0300

    CHANGES: change view on desktop and on web browser

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import os
 
 BASE_DIR = os.getcwd()
+attribs = "--host 192.168.0.112 --port 8080"
 
 
 def main():
-    command_ = 'flet ' + 'main.py' + ' -r'
+    command_ = f'flet main.py -r {attribs}'
+    print(command_)
     os.system(command_)
 
 
