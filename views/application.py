115dbc1f9b1f7b16784a8e3da4649abdc2abd198
spike <azamtoiri@gmail.com>
2023-12-17 12:47:02 +0300

    ADD: some comments

--- views/application.py
+++ views/application.py
@@ -1,7 +1,7 @@
 from flet import *
 
-from views.common_views import WelcomeView, LoginView, RegisterView
 from utils.handler import Handler
+from views.common_views import WelcomeView, LoginView, RegisterView
 from views.student_pages.student_main import StudentView
 
 
@@ -40,40 +40,43 @@ class Application:
                 break
 
     # region: Showing views
-    def show_register_view(self) -> None:
-        self.page.go(self.register_view.route)
-
     def show_login_view(self) -> None:
         self.page.go(self.login_view.route)
+
+    def show_register_view(self) -> None:
+        self.page.go(self.register_view.route)
 
     def show_welcome_view(self):
         self.page.go(self.welcome_view.route)
 
     def show_student_view(self):
         self.page.go(self.student_view.route)
+
     # endregion
 
-    @property
+    # region: @Properties
+    @property  # login_button for login
     def login_button(self) -> OutlinedButton:
         return self.login_view.login_button
 
-    @property
+    @property  # register_button for registering
     def register_button(self) -> TextButton:
         return self.register_view.register_button
 
-    @property
+    @property  # "Регистрация" button on login_view
     def not_registered_button(self):
         return self.login_view.create_account_button
 
-    @property
+    @property  # "Вход" button on register_view
     def already_registered_button(self):
         return self.register_view.login_button
 
-    @property
-    def welcome_register_button(self):
-        return self.welcome_view.register_button
-
-    @property
+    @property  # login button on welcome_view
     def welcome_login_button(self):
         return self.welcome_view.login_button
 
+    @property  # register button on welcome_view
+    def welcome_register_button(self):
+        return self.welcome_view.register_button
+
+    # endregion
