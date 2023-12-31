16bc37355dfa55bcfe8e96284c121faada1a0935
spike <azamtoiri@gmail.com>
2023-12-31 20:48:32 +0300

    add: Responsive row Example

--- test.py
+++ test.py
@@ -1,58 +1,40 @@
 import flet as ft
-import flet.canvas as cv
-
-
-class State:
-    x: float
-    y: float
-
-
-state = State()
 
 
 def main(page: ft.Page):
     page.title = "Flet Brush"
+    page.scroll = ft.ScrollMode.ALWAYS
 
-    def pan_start(e: ft.DragStartEvent):
-        state.x = e.local_x
-        state.y = e.local_y
-
-    def pan_update(e: ft.DragUpdateEvent):
-        cp.shapes.append(
-            cv.Line(
-                state.x, state.y, e.local_x, e.local_y, paint=ft.Paint(stroke_width=3)
-            )
-        )
-        cp.update()
-        state.x = e.local_x
-        state.y = e.local_y
-
-    cp = cv.Canvas(
-        [
-            cv.Fill(
-                ft.Paint(
-                    gradient=ft.PaintLinearGradient(
-                        (0, 0), (600, 600), colors=[ft.colors.CYAN_50, ft.colors.GREY]
-                    )
-                )
+    card = ft.Card(
+        col={"sm": 6, "md": 4},
+        content=ft.Container(
+            content=ft.Column(
+                [
+                    ft.ListTile(
+                        leading=ft.Icon(ft.icons.ALBUM),
+                        title=ft.Text("The Enchanted Nightingale"),
+                        subtitle=ft.Text(
+                            "Music by Julie Gable. Lyrics by Sidney Stein."
+                        ),
+                    ),
+                    ft.Row(
+                        [ft.TextButton("Buy tickets"), ft.TextButton("Listen")],
+                        alignment=ft.MainAxisAlignment.END,
+                    ),
+                ]
             ),
-        ],
-        content=ft.GestureDetector(
-            on_pan_start=pan_start,
-            on_pan_update=pan_update,
-            drag_interval=10,
-        ),
-        expand=False,
-    )
-
-    page.add(
-        ft.Container(
-            cp,
-            border_radius=5,
-            width=float("inf"),
-            expand=True,
+            width=400,
+            padding=10,
         )
     )
+    column = ft.ResponsiveRow(run_spacing=20)
+    for i in range(10):
+        column.controls.append(card)
+
+    text_field = ft.TextField()
+
+    page.add(column, text_field, ft.OutlinedButton(icon=ft.icons.ADD))
+    page.update()
 
 
 ft.app(main)
